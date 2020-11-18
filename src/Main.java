62b81e34c9244db6fd5ce578911693408e85ad3b
Stronfil <you@gmail.com>
2020-11-18 17:48:48 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void task(String[]args){
-        System.out.println("задание2");
+    public static void main(String[] args) {
+        System.out.println("задание 2");
     }
 }
