ef5bc7e21ad4597841815b63dbac337b88bbf0e0
Stronfil <tambovskii99@gmail.com>
2020-12-09 16:00:16 +0300

    hw6

---
+++ src/Cat.java
@@ -0,0 +1,15 @@
+public class Cat extends Animal{
+    private static int runMax=200;
+    private static int swimMax=0;
+     public Cat(String type,String name){
+         super(type,name,swimMax,runMax);
+
+
+
+
+
+
+     }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        employee[] are = new employee[5];
-            are[0] = new employee("Oleg Belov", "marketer", "belov@bk.ru", 73255, 300, 50);
-            are[1] = new employee("Valeria Oblomova", "accountant", "lera@bk.ru", 788483, 500, 46);
-            are[2] = new employee("Artem Lojkin", "lawyer", "lovis@bk.ru", 694499, 600, 32);
-            are[3] = new employee("Olga Sidorova", "interpreter", "Sidrrr@gmail.com ", 8345342, 800, 41);
-            are[4] = new employee("Nikolay Sveridov", "marketer", "Sver@gmail.com", 53342, 650, 28);
-            for (int i = 0; i < are.length; i++) {
-                if (are[i].getAge()>40){
-                    are[i].info();
-                }
-            }
+
+        System.out.println();
+
+       Animal dog=new Dog("Пес","Rex");
+       dog.distanceSwim(6);
+       dog.distanceRun(360);
+
+        System.out.println();
+
+
+
+
+        Animal cat=new Cat("Кот","Sem");
+        cat.distanceSwim(0);
+        cat.distanceRun(100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
     }
 
-}
 
 
 
